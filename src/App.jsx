7e9a8da8a842aac0a4e146b543a3b27361d593e9
Dev-J0 <dityx.obsidian@gmail.com>
2023-09-11 15:16:20 +0200

    working on header then footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import './styles/style.css'
+import Header from './components/Header'
 
 function App() {
   
   return (
     <>
-  
+   <div className="header">
+        <Header />
+  </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function Header() {
+    return (
+        <>
+      <h1>Resume</h1>
+      <h2>Please fill in the information to put together a professional CV!</h2>
+        </>
+    )
+}
+
+
+export default Header;
+
+
