fe588df3351b48559a9313b415305d524a429840
Dev-J0 <dityx.obsidian@gmail.com>
2023-09-12 15:31:28 +0200

    Done with basic info, put reset button as well, just have to style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import './styles/style.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import BasicInfo from './components/BasicInfo'
 
 function App() {
   
@@ -11,7 +11,7 @@ function App() {
    <div className="header">
         <Header />
   </div>
-
+    <BasicInfo />
 
 
 
@@ -24,3 +24,22 @@ function App() {
 }
 
 export default App;
+//Basic info
+//name
+//email
+//phone number
+//submit info button
+
+
+//Educational Experience
+//School name
+//title of study
+//date of study
+//submit info button
+
+//Practical experience
+//company name
+//position title
+//main responsabilities of your jobs
+//date from and until when you worked for that company
+//submit info butt
--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -1,4 +1,102 @@
-//First name
-//last name
-//title
-//little summary about self
+import { useState } from "react"
+
+function BasicInfo() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState ('');
+    const [title, setTitle] = useState ('');
+    const [summary, setSummary] = useState ('');
+    const [isRegistered, setIsRegistered] = useState(false);
+    
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSummaryChange = (event) => {
+        setSummary(event.target.value);
+    };
+
+    const handleFirstNameChange = (event) => {
+        setFirstName(event.target.value);
+    };
+
+    const handleLastNameChange = (event) => {
+        setLastName(event.target.value);
+    };
+
+    const handlePhoneNumberChange = (event) => {
+        setPhoneNumber(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setIsRegistered(true);
+    };
+
+   const handleEdit = () => {
+    setIsRegistered(false);
+   };
+
+   const handleReset = () => {
+    setTitle('');
+    setEmail(''),
+    setFirstName('');
+    setLastName('');
+    setPhoneNumber('');
+    setSummary('');
+   }
+
+    return (
+        <div className="Basic-Info">
+        {isRegistered ? (
+            <>
+            <p>Title : {title}</p>
+            <p>Summary {summary}</p>
+            <p>First Name : {firstName}</p>
+            <p>Last Name : {lastName}</p>
+            <p>Phone : {phoneNumber}</p>
+            <p>Email : {email}</p>           
+            <button onClick={handleEdit}>Edit</button> 
+            <button onClick={handleReset}>Reset</button>
+            </>
+        ) : (
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Title : <input type="text" value={title} onChange={handleTitleChange}
+                    />
+                </label>
+                <label>
+                    Summary: <input type="textarea" value={summary} onChange={handleSummaryChange}
+                    />
+                </label>
+                <label>
+                    First Name: <input type="text" value={firstName} onChange={handleFirstNameChange}
+                    />
+                </label>
+                <label>
+                   Last Name: <input type="text" value={lastName} onChange={handleLastNameChange}
+                    />
+                </label>
+                <label>
+                   Phone: <input type="number" value={phoneNumber} onChange={handlePhoneNumberChange}
+                    />
+                </label>
+                <label>
+                    Email: <input type="email" value={email} onChange={handleEmailChange}
+                    />
+                </label>
+                <button type="submit">Validate information</button>
+
+                </form>
+            )}
+        </div>
+    )
+}
+
+
+export default BasicInfo;
